1966efc95cc79e03ac7fc3643530ec0e27422de1
Amruni2616 <160886402+amrunimehta26@users.noreply.github.com>
2025-10-20 19:31:08 +0530

    Remove server start logic from server.js

--- server.js
+++ server.js
@@ -109,7 +109,4 @@ app.use('*all', async (req, res) => {
   }
 })
 
-// Start http server
-app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`)
-})
+export default app
