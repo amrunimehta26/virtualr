238527d0b73251bc4ce8c06ce3400e91c45725fc
Amruni2616 <160886402+amrunimehta26@users.noreply.github.com>
2025-10-20 22:10:10 +0530

    Enhance Vite config with build and SSR options
    Added build and SSR configuration to Vite.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+
+  build: {
+    outDir: 'dist',          // build output folder
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html')
+    }
+  },
+
+  ssr: {
+    noExternal: ['react', 'react-dom']  // bundle react for SSR
+  }
 })
